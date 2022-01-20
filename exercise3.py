757274e7bdf2a153e06bb64d23909e19a2814335
Group005BSE1 <97891906+Group005BSE1@users.noreply.github.com>
2022-01-20 20:43:12 +0530

    Add files via upload
    Assignments

---
+++ exercise3.py
@@ -0,0 +1,4 @@
+hours = input("Enter Hours: ")
+rate = input("Enter Rate: ")
+gross_pay = float(hours) * float(rate)
+print(gross_pay)
---
+++ exercise5.py
@@ -0,0 +1,3 @@
+s = input("What's your temperature in Celcius: ")
+Fahrenheit = float(s) * 1.8 + 32
+print(Fahrenheit)
