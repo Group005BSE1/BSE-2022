96e68024b6d91d0af9bee30333f66d70fe9b2209
Group005BSE1 <97891906+Group005BSE1@users.noreply.github.com>
2022-02-13 00:55:57 +0300

    Assignment 5

---
+++ exercise__3.py
@@ -0,0 +1,95 @@
+print("Welcome to the Vending machine change maker")
+print("Change maker initialised")
+a = [25, 25, 25, 0, 0]
+b = ["nickels", "dimes", "quarters", "ones", "fives"]
+c =["'n'", "'d'", "'q'", "'o'", "'f'", "'c'"]
+d = ["deposit a nickel", "deposit a dime", "deposit a quarter", "deposit a one dollar bill", "deposit a five dollar bill", "cancel the purchase"]
+e = [0.05, 0.1,0.25,1,5]
+count= 0
+def start(a,b):
+    print("Stock contains: ")
+    for i in range(len(a)):
+        print(a[i], "-", b[i])
+    return identifying_purchase_price(a)
+def identifying_purchase_price(a):
+    user_input = input("\nEnter the purchase price (xx.xx) or `q' to quit:")
+    if user_input =="q":
+        t = (a[0]*0.05 + a[1]*0.1 + a[2]* 0.25)
+        q = (a[4]*5 + a[3]*1)
+        if t*100 >= 100:
+            q += t//1
+            t -= t//1
+        print("Total: {0} dollars and {1} cents".format( q, int(t*100) ))
+    else:
+        checked_input = round(float(user_input), 2)
+        a = ((checked_input * 100) // 5)
+        if (((checked_input * 100)) - (a * 5)) == 0:
+            return deposit_menu(), depositing(checked_input)
+        else:
+            print("Illegal price: Must be a non-negative multiple of 5 cents.")
+            return identifying_purchase_price(a)
+
+def deposit_menu():
+    print("\nMenu for  deposits")
+    for i in range(len(c)):
+        print(c[i], "-", d[i])
+
+def depositing(y):
+    balance = y
+    while round(balance, 2) > 0:
+        print("\nPayment due: {0} dollars and {1} cents".format(int(balance), int(round(((balance - int(balance)) * 100), 0))))
+        deposit = input("Indicate your deposit:")
+        for (i,v) in enumerate(["n", "d", "q", "o", "f" ]):
+            if deposit == v:
+                a[i] += 1
+                balance -= e[i]
+                if balance <= 0:
+                    return returning_change(abs(balance),0)
+        if deposit == "c":
+            return returning_change(y, balance)
+        elif deposit not in "ndqofc":
+            print("Illegal selection:", deposit)
+
+def returning_change(s, v):
+    print("\nPlease take the change below")
+    cummulative_addition = s - v
+    cummulative_manager = 0
+    remainder = 0
+    while round(cummulative_addition, 2) > 0:
+        for (i) in range(3):
+            g = 0
+            z = round(cummulative_addition, 2) / e[2 - i]
+            if round(cummulative_addition, 2) != 0:
+                if z <= a[2 - i]:
+                    g = z - int(z)
+                    if int(z) > 0:
+                        print(int(z), b[2 - i],)
+                    cummulative_addition -= z * e[2 - i]
+                    a[2 - i] -= int(z)
+                    if g != 0:
+                        cummulative_manager += e[2 - i] * g
+                        cummulative_addition += e[2 - i] * g
+                elif z > a[2 - i]:
+                    print(a[2 - i], b[2 - i],)
+                    cummulative_addition -= a[2 - i] * e[2 - i]
+                    a[2 - i] -= a[2 - i]
+
+        break
+    remainder = round(cummulative_manager, 2)*100
+
+    if round(cummulative_addition, 2) > 0:
+        if int(cummulative_addition) != 0:
+            real_dollar = int(cummulative_addition)
+            if int(remainder) >= 100:
+                real_dollar += round(cummulative_manager, 2) / 1
+                cummulative_manager %= 1
+            print("Machine is out of change.\nSee store manager for remaining refund.\nAmount due is: {0} dollars {1} cents".format(real_dollar , round(cummulative_manager, 2) * 100))
+        else:
+            print("Machine is out of change.\nSee store manager for remaining refund.\nAmount due is: {0} cents".format(round(cummulative_manager, 2) * 100))
+        return "\n" , start(a,b)
+    elif round((cummulative_addition),2) == 0:
+        print("No change due\n")
+        return "\n" , start(a,b)
+    else:
+        return "\n" ,start(a,b)
+start(a,b)
