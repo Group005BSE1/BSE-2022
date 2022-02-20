d18326b56fd42598b7a54795dbea57bab9c37991
Group005BSE1 <97891906+Group005BSE1@users.noreply.github.com>
2022-02-20 23:20:04 +0300

    Chapter 2 Work

---
+++ chapter2/excercise6.py
@@ -0,0 +1,11 @@
+X1 = input("x1")
+X2 = input("x2")
+Y1 = input("y1")
+Y2 = input("y2")
+m = float(X1) - float(X2)
+n = float(Y1) - float(Y2)
+a = m**2
+b = n ** 2
+k = a + b
+d = k ** 0.5
+print(d)
---
+++ chapter2/exercise7.py
@@ -0,0 +1,44 @@
+Amount = input("Enter an amount to make change for: ")
+a = float(Amount)
+print("    ")
+print("Your change is")
+
+if a >= 20:
+    print(str(int(a // 20)) + " Twenties")
+else:
+    print("0 Twenties")
+a = a % 20
+if  a >= 10:
+    print(str(int(a // 10)) + " Tens")
+else:
+    print("0 Tens")
+a = a % 10
+if a >=5:
+    print(str(int(a // 5)) + " fives")
+else:
+    print("0 fives")
+a = a % 5
+if a >= 1:
+    print(str(int(a // 1)) + " Ones")
+else:
+    print("0 ones")
+a = a - int(a)
+if a >= 0.25:
+    print(str(int(a//0.25)) + " Quarters")
+else:
+    print("0 Quarters")
+a = a%0.25
+if  a >= 0.1:
+    print(str(int(a//0.1)) + " Dimes")
+else:
+    print("0 dimes")
+a = a%0.1
+if a >= 0.05:
+    print(str(int(a//0.05)) + " nickels")
+else:
+    print("0 nickels")
+a = a%0.05
+if a >=0.01:
+    print(str(int(a//0.01)) + " pennies")
+else:
+    print("0 pennies")
