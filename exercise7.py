dc2bc1ea091dbf6363300219fd234ba8e7a2125e
Group005BSE1 <97891906+Group005BSE1@users.noreply.github.com>
2022-02-20 19:05:59 +0300

    Chapter 2 work

--- exercise7.py
+++ exercise7.py
@@ -1,23 +1,44 @@
-try:
-    a = input("location: ")
-    b = int(input("pay: "))
-    if(a == "Mbarara"):
-        if(b >= 4000000):
-            print("without adoubt,i will take it")
-        else:
-            print("No thanks, I'll get a better one ")
-    elif(a == "Kampala"):
-        if(b >= 10000000):
-            print("without adoubt,i will take it")
-        else:
-            print("No thanks, I can find something better.")
-    elif(a == "Space"):
-        if(b >= 0):
-            print("Without adoubt,i will take it")
-    else:
-        if(b >= 6000000):
-            print("Sure, I can work with this")
-        else:
-            print("No thanks, I'll get a better one ")
-except:
-    print("Type valid data")
+Amount = input("Enter an amount to make change for: ")
+a = float(Amount)
+print("    ")
+print("Your change is")
+
+if a >= 20:
+    print(str(int(a // 20)) + " Twenties")
+else:
+    print("0 Twenties")
+a = a % 20
+if  a >= 10:
+    print(str(int(a // 10)) + " Tens")
+else:
+    print("0 Tens")
+a = a % 10
+if a >=5:
+    print(str(int(a // 5)) + " fives")
+else:
+    print("0 fives")
+a = a % 5
+if a >= 1:
+    print(str(int(a // 1)) + " Ones")
+else:
+    print("0 ones")
+a = a - int(a)
+if a >= 0.25:
+    print(str(int(a//0.25)) + " Quarters")
+else:
+    print("0 Quarters")
+a = a%0.25
+if  a >= 0.1:
+    print(str(int(a//0.1)) + " Dimes")
+else:
+    print("0 dimes")
+a = a%0.1
+if a >= 0.05:
+    print(str(int(a//0.05)) + " nickels")
+else:
+    print("0 nickels")
+a = a%0.05
+if a >=0.01:
+    print(str(int(a//0.01)) + " pennies")
+else:
+    print("0 pennies")
