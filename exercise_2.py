acde538e9e9dc2713a8f584701a3295a1f01554c
Group005BSE1 <97891906+Group005BSE1@users.noreply.github.com>
2022-02-20 23:23:09 +0300

    Delete exercise_2.py

--- exercise_2.py
+++
@@ -1,9 +0,0 @@
-def return_investment(a, b, c, d ):
-    return round((a * ((1 + b / d) ** (d * c))), 2)
-
-C = float(input("Initial investment: "))
-r = float(input("Rate: "))
-t = float(input("time in years"))
-n = float(input("No of times interest given per year: "))
-
-print(return_investment(C, r , t, n))
