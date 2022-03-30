2b494c4acee34c0c20d79825c4b4a73b1cacc4c7
Group005BSE1 <97891906+Group005BSE1@users.noreply.github.com>
2022-03-30 19:45:04 +0300

    Update exercise1.py

--- chapter5/exercise1.py
+++ chapter5/exercise1.py
@@ -1,13 +1,13 @@
-a=0
+sum=0
 count=0
 while True:
     try:
         num=input("enter the number")
         if num == "done":
             break
-        a+= int(num)
+        sum+= int(num)
         count +=1
     except:
         print("invalid input")
-b = (a/count)
-print(a, count, b)
+average = (sum/count)
+print(sum, count, average)
